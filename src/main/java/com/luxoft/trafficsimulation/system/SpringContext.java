5a5477c4e0170c68177e8fb58ce17bd05b72d0f1
Garip Tipici <gariptipici@gariptipici.local>
2019-01-14 22:44:10 +0300

    text

--- src/main/java/com/luxoft/trafficsimulation/system/SpringContext.java
+++ src/main/java/com/luxoft/trafficsimulation/system/SpringContext.java
@@ -6,6 +6,8 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 import com.luxoft.trafficsimulation.publishers.EventPublisher;
+import com.luxoft.trafficsimulation.repository.IVehicleService;
+import com.luxoft.trafficsimulation.repository.VehicleService;
 
 @Component
 public class SpringContext implements ApplicationContextAware {
@@ -14,6 +16,10 @@ public class SpringContext implements ApplicationContextAware {
  
     public static EventPublisher getEventPublisher() {
         return (EventPublisher)context.getBean("eventPublisher");
+    }
+    
+    public static IVehicleService getVehicleService() {
+    	return (VehicleService)context.getBean("vehicleService");
     }
      
     @Override
